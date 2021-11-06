e292d6aa48539fda71772573128ce2115610d9ee
hrand1005 <herbie@VENGEANCE.localdomain>
2021-11-06 13:31:32 -0400

    ADD: encoding step in compile_clips.sh, vids now compile correctly!

--- compile_clips.sh
+++ compile_clips.sh
@@ -1,1 +1,11 @@
-ffmpeg -f concat -i clips.txt -c copy compiled-vid.mp4
+CLIPS="./db/tmp/*.mp4"
+for file in $CLIPS
+do
+    echo "Formatting $file"
+    basename="${file%.*}"
+    # re-encode files to a consistent format
+    ffmpeg -i $file -c:v libx264 -preset slow -crf 22 -c:a copy $basename.mkv
+    #clips.txt used for concatenation step
+    echo "file '$basename.mkv'" >> clips.txt
+done
+ffmpeg -f concat -safe 0 -i clips.txt compiled-vid.mp4
--- nuke_data.sh
+++ nuke_data.sh
@@ -1,4 +1,5 @@
 # this script should nuke downloaded clips as well as the .db files that manage them
 rm db/tmp/*
 rm db/*.db
+rm clips.txt
 rm compiled-vid.mp4
