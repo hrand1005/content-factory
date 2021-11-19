2146e8776007473e3ae4615fa266b1ddbae4e7ea
Herbie <hjrand1005@gmail.com>
2021-11-18 22:14:25 -0500

    ADD: ffmpeg overlay attempt

--- compile_clips.sh
+++ compile_clips.sh
@@ -5,6 +5,9 @@ do
     echo "Formatting $file"
     basename="${file%.*}"
     # re-encode files to a consistent format
+    ffmpeg -i $file -vf
+    "drawtext=fontfile=/usr/share/fonts-droid-fallback/truetype/DroidSansFallback.ttf:text='TEXT':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=0:y=0"
+    -codec:a copy output.mp4 
     ffmpeg -i $file -c:v libx264 -preset slow -crf 22 -c:a copy $basename.mkv
     #clips.txt used for concatenation step
     echo "file '$basename.mkv'" >> clips.txt
