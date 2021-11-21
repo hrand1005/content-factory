88540be6e763e30e0eed9d1f6c634401f88ac8d4
Herbie <hjrand1005@gmail.com>
2021-11-20 21:18:18 -0500

    FIX: spacing, code cleanup

--- main.py
+++ main.py
@@ -4,26 +4,32 @@ from clip import content
 from db import db
 from compile import compile
 
-CLIP_DIR = "./db/tmp/"
 
+RAW_DIR = "db/tmp/"
+PROCESSED_DIR = "compile/tmp/"
+FINAL_PRODUCT = "compiled-vid.mp4"
+
+
+# parses args, ie preset
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Download clips, compile them into a video, and upload it to youtube.")
     parser.add_argument('--preset',
         help="Name of the preset with configurations for content regurgitation.")
     args = parser.parse_args()
-
     return args.preset.lower()
 
+
+# prints the number and paths of the downloaded content
 def print_status(status_obj):
     print(f"\nSuccessfully yoinked {len(status_obj)} objects:")
     for status in status_obj:
         print(f"{status}")
 
-def main():
-    preset = parse_args()
 
+def main():
     # TODO: Find out a better way to select strategies at runtime
+    preset = parse_args()
     if preset == "premiummelee":
         strategy = content.PremiumMelee()
     elif preset == "darksouls": 
@@ -33,68 +39,56 @@ def main():
     else:
         print(f"Strategy '{preset}' not found. \nExiting...")
         exit(1)
-
+    # initialize database
     database = db.ClipDatabase(preset)
-    clips = strategy.fetch_clips()
 
+    # fetch clips using strategy
+    clips = strategy.fetch_clips()
     if len(clips) == 0:
         print("No new clips to fetch for PremiumMelee.\nExiting...")
         exit(1)
 
+    # verify that the clips don't already existin the database
     verified_clips = database.verify_clips(clips)
-    print(f"Found {len(verified_clips)} verified clips, tryna yoink.")
-    
     if len(verified_clips) == 0:
         print("Retrieved clips already exist in the db.\nExiting...")
         exit(1)
 
-    dl_status = content.download_clips(verified_clips, CLIP_DIR)
+    # download the verified clips, print the status
+    dl_status = content.download_clips(verified_clips, RAW_DIR)
     print_status(dl_status)
-
-    # TODO: is this the right place to do this? --> may want to move to after publishing step
-    database.insert_clips(verified_clips)
 
     # prompts user to manually compile -- used for testing
     print(f"\nLatest clips yoinked for {preset}!")
     compile_flag = input("Ready to edit and compile? [Y/N]\n")
-
     if compile_flag.lower()[0] == "y":
         edited_clips = []
-        print("applying overlays...")
         for clip in verified_clips: 
-            out_dir = "compile/tmp/"
-            # TODO: unify this logic somewhere. Maybe a class for a clip object so
-            # we can have one thing to refer to? think: verified flags, name,
-            # filename, preprocessed path, postprocessed path, etc.
-            clip_url = clip['url']
-            filename = f"{clip_url.rpartition('/')[-1]}.mp4"
-
-            clip_path = f"{CLIP_DIR}{filename}"
-            edited_clip = compile.apply_overlay(clip, clip_path, out_dir)
+            filename = f"{clip['url'].rpartition('/')[-1]}.mp4"
+            clip_path = f"{RAW_DIR}{filename}"
+            # apply text overlay here, other processing should happen here
+            edited_clip = compile.apply_overlay(clip, clip_path, PROCESSED_DIR)
             edited_clips.append(edited_clip)
-
-        print("compiling...")
-        print(f"compiling these clips: {edited_clips}")
         compile.compile_clips(edited_clips)
-        # os.system("./compile_clips.sh")
     else:
         print("Exiting without compiling...")
         exit(0)
 
-    #then upload vid
-    #then delete the local clips, but keep the vid
-
     # prompts user to manually upload -- used for testing
     print(f"\nVid compiled for {preset}!")
     upload_flag = input("Ready to upload? [Y/N]\n")
-
     if upload_flag.lower()[0] == "y":
-        print("Uploading...")
-        cmd = f"python3 share/share.py --file compiled-vid.mp4 --privacyStatus private --title {preset}-prototype"
-        os.system(cmd)
+        file_arg = f"--file {FINAL_PRODUCT}"
+        priv_arg = "--privacyStatus private"
+        title_arg = f"--title {preset}-prototype"
+        os.system(f"python3 share/share.py {file_arg} {priv_arg} {title_arg}")
     else:
         print("Exiting without uploading...")
         exit(0)
+
+    # add the clips from the uploaded vid to the database
+    # TODO: decide how to handle verified clips that haven't been downlaoded
+    database.insert_clips(verified_clips)
 
 if __name__ == "__main__":
     main()
